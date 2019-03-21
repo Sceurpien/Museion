fc4cbd1c01c8a3138ca5e373c420026b075651ca
w16-cfr\Gabor Bata Kovacs <gabor.batakovacs@castlefrog.fr>
2019-03-21 12:15:35 +0100

    Designs

--- Museion.Vue/Scripts/site.js
+++ Museion.Vue/Scripts/site.js
@@ -1,2 +1,7 @@
-﻿Vue.component('vuetest', require('./vuetest.js'));
+﻿import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+
+Vue.component('vuetest', require('./vuetest.js'));
 Vue.component('login', require('./login.js'));
